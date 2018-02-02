14fb0a0c600afec45487ef2ab77daa2f80842b13
Wu Minzhe <wuminzhe@126.com>
2018-02-02 14:01:04 +0800

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Lobsters::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'bitip.in'], protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'bitip.in', protocol: 'https' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
